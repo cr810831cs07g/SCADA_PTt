f425d9dac416cafa9398e9be5cad0489224e6e1a
cr810831cs07g <cr810831.cs07g@nctu.edu.tw>
2020-07-12 04:39:27 -0700

    dubug

--- smod/Application/modules/modbus/scanner/fullscan.py
+++ smod/Application/modules/modbus/scanner/fullscan.py
@@ -117,7 +117,7 @@ class Module:
    				writer.writerows(row_report)
 			#print('support: ' ,supportfunccode)
 			#print('probably: ',probablyfunccode)
-		print(row_report)
+		print('===FULLSCAN FINISH===')
             
             
             
